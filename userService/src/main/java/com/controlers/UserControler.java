ba321f2aa93d7a28473caee389bd2722cc7104b3
Shawaiz Khan <shawaizkhan@cdacnoida.in>
2025-10-24 12:31:28 +0530

    add circuit breaker

--- userService/src/main/java/com/controlers/UserControler.java
+++ userService/src/main/java/com/controlers/UserControler.java
@@ -2,6 +2,8 @@ package com.controlers;
 
 import java.util.List;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -40,14 +42,24 @@ UserService userService;
 	return	ResponseEntity.ok(saveUser);
 	}
 	
-	
+int retryCount = 0;
 	@GetMapping("/{userId}")
+	@CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallbackMethod")
+	@Retry(name = "ratingHotelService",fallbackMethod = "ratingHotelFallbackMethod")
 	ResponseEntity<User> getUser(@PathVariable String userId){
+		System.err.println("-------------"+retryCount++);
 		System.out.println("get user method is calling");
 		User saveUser = userService.getUser(userId);
 	
 	return	ResponseEntity.ok(saveUser);
 	}
+
+	ResponseEntity<User> ratingHotelFallbackMethod(String userId,Exception e) {
+		System.err.println("rating hotel fallback method is called because service is down : "+e.getMessage());
+	User dummy = User.builder().userAbout("dummy").userName("dummy name").build();
+	return ResponseEntity.ok(dummy);
+	}
+
 	
 	@GetMapping("/test")
     public String testServer() {
